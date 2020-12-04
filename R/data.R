7f5af9d829b33dd21d691567b75cc2f9d95acbba
Alvaro Franquet <alvarofranq@gmail.com>
2020-12-04 11:35:55 +0100

    Add alt_emporda data

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Electronic medical record of 66292 people for type 2 diabetes mellitus
+#'
+#' A dataset containing survival data for 66292 individuals with or without diabetes mellitus type 2
+#'
+#' @format A data frame with 66292 rows and 7 variables:
+#' \describe{
+#'   \item{id}{ a character string used as individual identifier}
+#'   \item{sex}{an integer indicating the individual's sex}
+#'   \item{dm2}{an integer indicating if the individuals has diabetes mellitus type 2 (1) or not (0) }
+#'   \item{dm2_date}{The date the diabetes was diagnosed}
+#'   \item{birth_year}{an integer indicating the year of birth}
+#'   \item{is_dead}{an integer indicating if the individual is dead (1) or not (0)}
+#'   \item{death_date}{when the individual dies}
+#' }
+#' @source \url{https://aquas.gencat.cat/es/ambits/analitica-dades/padris/}
+"alt_emporda"
